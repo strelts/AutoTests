4b18963f599b5cc4ea1e30efaead0d88f1cf0f9b
Artem <Artem@DESKTOP-EH1B5C6>
2025-02-26 16:37:15 +0300

    Добавьте файлы проекта.

---
+++ AutoTestsColvir.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace ColvirAutoTests
+{
+    public class Colvir
+    {   //Адрес сервиса колвир
+        static String ColvirUrl = "http://localhost:8080"; 
+        //Path запроса для поиска информации о кредите по иин
+        static String loansEndpoint = ColvirUrl + "/ekassir/client/loans";
+        //Path запроса для поиска информации по счетам по иин
+        static String accListEndpoint = ColvirUrl + "/ekassir/client/acclist";
+        public String GetColvirUrl()
+        {
+            return ColvirUrl;
+        }
+        public String GetLoansEndPoint()
+        {
+            return loansEndpoint;
+        }
+        public String GetaccListEndpoint()
+        {
+            return accListEndpoint;
+        }
+
+
+
+    }
+
+ 
+
+    [TestClass]
+    public sealed class ColvirAutoTests
+    {
+        Colvir Colvir = new Colvir();
+
+       
+
+        [TestMethod]
+        public async Task TestSuccessCheckByIIN()
+        {
+         
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    iin = "111111111111",
+                    term_id = "204204"
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+                client.DefaultRequestHeaders.Add("apiKey", "a785d625-d57b-4a1a-9ef4-8b83568dd228");
+                HttpResponseMessage response = await client.PostAsync(Colvir.GetLoansEndPoint(), content);
+                
+               
+                //Проверяю статус код ответа
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+
+                    //Проверяю параметр name
+                    Assert.AreEqual("OK", parsedResponse.message.ToString());
+                    Assert.AreEqual(200, (int)parsedResponse.status);
+                    Console.WriteLine(parsedResponse);
+             
+
+                }
+
+
+            }
+        } // Конец метода
+
+        [TestMethod]
+        public async Task TestNotSuccessCheckByIIN1() //Проверка что сервис отвечает с ошибкой если обязательный параметр имеет неверный тип данных
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    iin = 122222222222,
+                    term_id = "204204"
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+                client.DefaultRequestHeaders.Add("apiKey", "a785d625-d57b-4a1a-9ef4-8b83568dd228");
+                HttpResponseMessage response = await client.PostAsync(Colvir.GetLoansEndPoint(), content);
+
+                //Проверяю статус код ответа
+                Assert.AreNotEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+
+                    //Проверяю параметр name
+                    Assert.AreNotEqual("OK", parsedResponse.message.ToString());
+                    Assert.AreNotEqual(200, (int)parsedResponse.status);
+                    Console.WriteLine(parsedResponse);
+
+
+                }
+
+
+            }
+        } // Конец метода
+        [TestMethod]
+        public async Task TestNotSuccessCheckByIIN2() //Проверка что сервис отвечает с ошибкой если отсутстует обязательный параметр
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    iin = "111111111111"
+                    
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+                client.DefaultRequestHeaders.Add("apiKey", "a785d625-d57b-4a1a-9ef4-8b83568dd228");
+                HttpResponseMessage response = await client.PostAsync(Colvir.GetLoansEndPoint(), content);
+
+                //Проверяю статус код ответа
+                Assert.AreNotEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+
+                    //Проверяю параметр name
+                    Assert.AreNotEqual("OK", parsedResponse.message.ToString());
+                    Assert.AreNotEqual(200, (int)parsedResponse.status);
+                    Console.WriteLine(parsedResponse);
+
+
+                }
+
+
+            }
+        } // Конец метода
+
+        [TestMethod]
+        public async Task ПроверкаЧтоСервисОтветчаетОшибкойВслучаеЕслиОтсутствуетApiKey()
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    iin = "111111111111",
+                    term_id = "204204"
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+              //  client.DefaultRequestHeaders.Add("apiKey", "a785d625-d57b-4a1a-9ef4-8b83568dd228");
+                HttpResponseMessage response = await client.PostAsync(Colvir.GetLoansEndPoint(), content);
+
+                //Проверяю статус код ответа
+                Assert.AreNotEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+
+                    //Проверяю параметр name
+                    Assert.AreNotEqual("OK", parsedResponse.message.ToString());
+                    Assert.AreNotEqual(200, (int)parsedResponse.status);
+                    Console.WriteLine(parsedResponse);
+
+
+                }
+
+
+            }
+        } // Конец метода
+
+        [TestMethod]
+        public async Task ПроверкаЧтоСервисОтветчаетОшибкойВслучаеЕслиApiKeyНекорректный()
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    iin = "111111111111",
+                    term_id = "204204"
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+                client.DefaultRequestHeaders.Add("apiKey", "we2385d625-d57b-4a1a-9ef4-8b83568dd228"); //Добавляю неверный ApiKey
+                HttpResponseMessage response = await client.PostAsync(Colvir.GetLoansEndPoint(), content);
+
+                //Проверяю статус код ответа
+                Assert.AreNotEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+
+                    //Проверяю параметр name
+                    Assert.AreNotEqual("OK", parsedResponse.message.ToString());
+                    Assert.AreNotEqual(200, (int)parsedResponse.status);
+                    Console.WriteLine(parsedResponse);
+
+
+                }
+
+
+            }
+        } // Конец метода
+
+
+
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace TestProject1
+{
+
+    public class TestTools
+    {
+       public async Task<dynamic> ReadAndDeserializeResponseAsync(HttpResponseMessage response)
+    {
+        if (response == null)
+        {
+            throw new ArgumentNullException(nameof(response), "Ответ не может быть null.");
+        }
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException($"Запрос завершился с кодом {response.StatusCode}.");
+        }
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject(responseContent);
+    }
+    }
+   // [TestClass]
+    public sealed class Example_Tests
+    {
+        [TestMethod]
+        public async Task FirstTest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new { 
+                    name2 = "Artem123" 
+                };
+                
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+               // HttpResponseMessage response = await client.PostAsync("http://localhost:8080/ekassir", content);
+                HttpResponseMessage response = await client.GetAsync("https://api.agify.io/?name=Artem");
+                //Проверяю статус код ответа
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+                    
+                    //Проверяю параметр name
+                   Assert.AreEqual("Artem", parsedResponse.name.ToString());
+                   Assert.AreEqual(39, (int)parsedResponse.age);
+                    Console.WriteLine(parsedResponse);
+
+                }
+
+
+            }
+        }
+
+
+
+        [TestMethod]
+        public async Task SecondTest()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //Создаю тело запроса
+                var RequestBody = new
+                {
+                    TestNotFound = "TestNotFound"
+                };
+
+                var json = JsonConvert.SerializeObject(RequestBody);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                //Отправля Post запрос с телом запроса 
+                HttpResponseMessage response = await client.PostAsync("http://localhost:8080/ekassir", content);
+                //Проверяю статус код ответа
+                Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic parsedResponse = JsonConvert.DeserializeObject(responseContent);
+                    //Проверяю параметр name
+                    //Assert.AreEqual("Артём", parsedResponse.name.ToString());
+
+                }
+
+
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
